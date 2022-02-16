4dd948b60b3cf0dc0401249b30254458fd2478ec
Shree <bornarekalpesh@gmail.com>
2022-02-16 10:38:03 +0530

    Bottom Navigation Implemented

--- app/src/main/java/com/example/navcomponentdemo/MainActivity.kt
+++ app/src/main/java/com/example/navcomponentdemo/MainActivity.kt
@@ -2,29 +2,20 @@ package com.example.navcomponentdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val navController=Navigation.findNavController(this,R.id.nav_host_fragment)
-        //setupBottomNavigationMenu(navController)
-
-
-    }
-    fun setupBottomNavigationMenu(navController: NavController){
-        R.id.bottom_nav.let {
-          //  NavigationUI.setupWithNavController(it, navController)
+        findViewById<BottomNavigationView>(R.id.bottom_nav)?.let {
+            NavigationUI.setupWithNavController(it, navController)
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return super.onSupportNavigateUp()
-    }
 }
